1ae405f20abd8c95b85ee312b87179a6fd541426
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-03-01 13:39:43 -0300

    Add develop server script

---
+++ start-devel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Run this script to start a local development server.
+# First you must fill you github application credentials below.
+#
+
+export GITHUB_CLIENT_ID=
+export GITHUB_CLIENT_SECRET=
+export OO_INDEX_GITHUB_USERNAME=openshift
+export OO_INDEX_GITHUB_REPONAME=oo-index
+export OO_INDEX_QUICKSTART_JSON=quickstart.json
+
+if [ -z "$GITHUB_CLIENT_ID" -o -z "$GITHUB_CLIENT_SECRET" ]; then
+	echo "Fill in your github credentials and try again."
+	exit 1
+fi
+
+set -e
+
+[ -d virtenv ] || virtualenv virtenv
+. virtenv/bin/activate
+egrep -o '[a-zA-Z0-9-]+==[0-9.]+' setup.py | xargs pip install
+
+python wsgi/myflaskapp.py
